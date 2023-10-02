807c7bba8967826c8c6b9d24750d00e0b3c194ee
GanapathyMano5858 <GanapathyMano04@gmail.com>
2023-10-02 22:18:21 +0530

    hi this is kirthana

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import Logo from "../../img/PingME_Logo.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navBarSection">
+      <div className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <img src={Logo} alt="Logo" style={{ width: "250px" }} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
