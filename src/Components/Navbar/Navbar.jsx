d84712f6cd8d6c329e7e8f4c62e1f9961782139c
GanapathyMano5858 <GanapathyMano04@gmail.com>
2023-10-02 22:39:47 +0530

    hi this is mano

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,77 @@
 import React from "react";
 import "./Navbar.css";
 import Logo from "../../img/PingME_Logo.svg";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
+
 const Navbar = () => {
   return (
-    <div className="navBarSection">
-      <div className="header flex">
+    <section className="navBarSection">
+      <header className="header flex">
         <div className="logoDiv">
           <a href="#" className="logo flex">
             <img src={Logo} alt="Logo" style={{ width: "250px" }} />
           </a>
         </div>
-      </div>
-    </div>
+
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+            <button className="btn">
+              <a href="#">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div className="closeNavbar">
+            <AiOutlineCloseSquare className="icon" />
+          </div>
+
+          <div className="toggleNavbar">
+            <TbGridDots className="icon" />
+          </div>
+        </div>
+      </header>
+    </section>
   );
 };
 
